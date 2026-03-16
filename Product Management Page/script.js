6bff7c22f0beb3a6974a6ba808e84f4e79816bdd
Tahmid Tasin <shawontor@gmail.com>
2026-03-16 21:15:50 +0600

    Commiting

--- Product Management Page/script.js
+++ Product Management Page/script.js
@@ -2,7 +2,31 @@
 
 let btn = document.getElementById('btn');
 
-btn.addEventListener('click', ()=>{
-    document.getElementById('cls').classList.remove('hidden');
 
+
+
+
+btn.addEventListener('click', ()=>{
+
+    let field = document.getElementById('field');
+    let qty = field.value;
+    let total;
+    qty = parseInt(qty);
+    if(qty<0)
+    {
+        field.value=0;
+        alert("Enter logical Quantity");
+    }
+
+    else{
+            document.getElementById('cls').classList.remove('hidden');
+            total = qty * 150;
+          let li =  document.createElement('li');
+          li.innerHTML = "Cheese\t\tX"+`${qty}`+"\t\t\t"+`${total}`+"Tk";
+          document.querySelector('.listclass').appendChild(li);
+    }
+
+
+    console.log(field.value);
+    console.log(total);
 })
